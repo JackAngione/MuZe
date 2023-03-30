793bc5e900ec846e12b7df674107161541407722
Paulo Vallecillo <pvalle3@students.towson.edu>
2023-03-30 16:37:18 -0400

    Working permission request before building music list

--- MuZeCode/app/src/main/java/com/example/muzecode/MainActivity.kt
+++ MuZeCode/app/src/main/java/com/example/muzecode/MainActivity.kt
@@ -7,6 +7,8 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.Settings
+import android.provider.Settings.*
+import android.util.Log
 import android.widget.SeekBar
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -17,8 +19,11 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -28,6 +33,7 @@ import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
 import androidx.media3.ui.PlayerView
 import com.example.muzecode.ui.theme.MuZeCodeTheme
+import com.google.accompanist.permissions.*
 import kotlinx.coroutines.delay
 import java.io.File
 
@@ -41,7 +47,7 @@ class MainActivity : ComponentActivity() {
 
         val mediaSession = MediaSession.Builder(this, player).build()
 
-        if (Build.VERSION.SDK_INT >= 30) {
+        /*if (Build.VERSION.SDK_INT >= 30) {
             if (hasFilesPermission()) {
                 Toast.makeText(this, R.string.perm_granted, Toast.LENGTH_LONG)
                     .show()
@@ -53,7 +59,7 @@ class MainActivity : ComponentActivity() {
                     uri
                 )
             )
-        }
+        } //should auto grant if <30*/
 
             //END CREATE MEDIA PLAYER
         setContent {
@@ -62,7 +68,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
+                ) {/*
                     AndroidView(
                         factory = { context ->
                             val audioFile = File("./Crystal Castles - Leni.mp3")
@@ -78,22 +84,94 @@ class MainActivity : ComponentActivity() {
                         update = { playerView ->
                             // Update the view if needed
                         }
-                    )
+                    )*/
                     val strings = listOf("Hello", "World", "Jetpack", "Compose", "hello", "Hello", "World", "Jetpack", "Compose", "hello", "Hello", "World", "Jetpack", "Compose", "hello")
-                    Greeting(player)
+                    permCheck(player)
                 }
             }
         }
     }
+    /*
     @RequiresApi(Build.VERSION_CODES.R)
-    private fun hasFilesPermission() = Environment.isExternalStorageManager()
+    private fun hasFilesPermission() = Environment.isExternalStorageManager()*/
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+/*
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+private fun checkPermission() {
+    val storagePermissionState = rememberPermissionState(android.Manifest.permission.MANAGE_EXTERNAL_STORAGE)
+    Log.d(null, "Value of storagePermission: ${storagePermissionState.status.isGranted}")
+    if(storagePermissionState.status.isGranted) {
+        Text("Storage Permission Granted!")
+    }else {
+        Column {
+            val storageError = if(storagePermissionState.status.shouldShowRationale){
+                "Storage access is needed for this app. Please grant permission."
+            }else {
+                "Storage Permission required for the app to function! Please grant the permission."
+            }
+            Text(storageError)
+            Log.d(null, "Should show prompt here...")
+            Button(onClick = { storagePermissionState.launchPermissionRequest() }) {
+                Text("Request permission")
+            }
+        }
+    }
+}*/
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+private fun permCheck(playerToPass: ExoPlayer) {
+    var doNotShow by rememberSaveable{ mutableStateOf(false)}
+    val storagePermissionState = rememberPermissionState(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+    val context = LocalContext.current
+    PermissionRequired(
+        permissionState = storagePermissionState,
+        permissionNotGrantedContent = {
+            //Runs if the specified permission is not granted
+            if(doNotShow){
+                Text("App Unavailable")
+            } else {
+                Column {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "The App makes use of your device's internal storage. Grant permission?"
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row {//creates a pop-up that automatically edits storage permission
+                        Button(onClick = {storagePermissionState.launchPermissionRequest()}){
+                            Text("OK")
+                        }
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Button(onClick = {doNotShow = true}) {
+                            Text("No Thanks")
+                        }
+                    }
+                }
+            }
+        },
+        permissionNotAvailableContent = { //shouldn't be an issue but is a failsafe for unavailable permission
+            Column {
+                Text( "Storage Access Denied! Please grant access via app settings!")
+                Spacer(modifier = Modifier.height(8.dp))
+                Button(onClick = {
+                    val i = Intent(ACTION_LOCATION_SOURCE_SETTINGS)
+                    context.startActivity(i)
+                }){
+                    Text("Go to Settings")
+                }
+            }
+        }
+    ) { //will run once permission is granted
+        Greeting(playerToPass)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @Composable
 fun Greeting(player: ExoPlayer) {
     var isPlaying by remember { mutableStateOf(false) }
-
 
     BottomSheetScaffold(
 
@@ -173,6 +251,8 @@ fun Greeting(player: ExoPlayer) {
         {
             derivedStateOf { currentFolder.listFiles() }
         }
+
+        //if permissions not yet granted, display nothing but prompt?
 
         LazyColumn(
             modifier = Modifier.fillMaxSize()
