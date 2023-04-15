067ddc8135750208656265d160205089acd1f0df
JackAngione <jangio1@students.towson.edu>
2023-04-15 16:58:00 -0400

    added dropdown menu on songs. *add to queue has no functionality

--- MuZeCode/app/src/main/java/com/example/muzecode/UI_views.kt
+++ MuZeCode/app/src/main/java/com/example/muzecode/UI_views.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -27,95 +29,96 @@ class UI_views {
         playerFunctionality: PlayerFunctionality
     )
     {
-        /*
-        val playerFunctionality = remember {
-
-            PlayerFunctionality()
-        }
-        */
-            //START FOLDER LIST UI
-            LazyColumn(
-                modifier = Modifier.fillMaxSize()
-            ) {
+        //START FOLDER LIST UI
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
 
 
-                item {
+            item {
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {
+                        if (playerFunctionality.currentFolder != File("/storage/emulated/0/Music")) {
+                            playerFunctionality.currentFolder =
+                                playerFunctionality.currentFolder.parentFile as File
+                            playerFunctionality.playingSongIndex = 0
+                        }
+                    })
+                {
+                    Text(
+                        text = "-Back-",
+                        fontSize = 18.sp,
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .align(Alignment.CenterHorizontally)
+                    )
+                }
+            }
+
+            itemsIndexed(playerFunctionality.currentFolderAudioFiles) { index, audioFileCard ->
+                if (audioFileCard.extension in arrayOf(
+                        "mp3",
+                        "wav",
+                        "ogg",
+                        "aac"
+                    ) || audioFileCard.isDirectory
+                ) {
                     Card(
                         modifier = Modifier.fillMaxWidth(),
                         onClick = {
-                            if (playerFunctionality.currentFolder != File("/storage/emulated/0/Music")) {
-                                playerFunctionality.currentFolder =
-                                    playerFunctionality.currentFolder.parentFile as File
-                                playerFunctionality.playingSongIndex = 0
+
+                            if (audioFileCard.isDirectory) {
+                                playerFunctionality.currentFolder = audioFileCard
+                            } else {
+                                playerFunctionality.playingFolderAudioFiles = playerFunctionality.currentFolderAudioFiles
+                                setPlayerQueue(
+                                    player = player,
+                                    playerFunctionality = playerFunctionality,
+                                    index
+                                )
                             }
-                        })
-                    {
+                        }) {
+                        Row(modifier = Modifier
+                            .padding(16.dp)
+                            .align(Alignment.CenterHorizontally))
+                        {
+                            Text(
+                                text = audioFileCard.name,
+                                modifier = Modifier.widthIn(max = 320.dp),
+                                fontSize = 18.sp,
+
+                                )
+                            Spacer(modifier = Modifier.weight(1f))
+                            TrackDropDownMenu()
+                        }
+                    }
+                }
+            }
+            itemsIndexed(playerFunctionality.currentFolderFiles) { index, audioFileCard ->
+                if (audioFileCard.isDirectory) {
+                    Card(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = {
+                            playerFunctionality.currentFolder = audioFileCard
+
+                        }) {
                         Text(
-                            text = "-Back-",
+                            text = audioFileCard.name,
                             fontSize = 18.sp,
                             modifier = Modifier
                                 .padding(16.dp)
                                 .align(Alignment.CenterHorizontally)
                         )
+
                     }
                 }
-
-                itemsIndexed(playerFunctionality.currentFolderAudioFiles) { index, audioFileCard ->
-                    if (audioFileCard.extension in arrayOf(
-                            "mp3",
-                            "wav",
-                            "ogg",
-                            "aac"
-                        ) || audioFileCard.isDirectory
-                    ) {
-                        Card(
-                            modifier = Modifier.fillMaxWidth(),
-                            onClick = {
-
-                                if (audioFileCard.isDirectory) {
-                                    playerFunctionality.currentFolder = audioFileCard
-                                } else {
-                                    playerFunctionality.playingFolderAudioFiles = playerFunctionality.currentFolderAudioFiles
-                                    setPlayerQueue(
-                                        player = player,
-                                        playerFunctionality = playerFunctionality,
-                                        index
-                                    )
-                                }
-                            }) {
-                            Text(
-                                text = audioFileCard.name,
-                                fontSize = 18.sp,
-                                modifier = Modifier
-                                    .padding(16.dp)
-                                    .align(Alignment.CenterHorizontally)
-                            )
-                        }
-                    }
-                }
-                itemsIndexed(playerFunctionality.currentFolderFiles) { index, audioFileCard ->
-                    if (audioFileCard.isDirectory) {
-                        Card(
-                            modifier = Modifier.fillMaxWidth(),
-                            onClick = {
-                                playerFunctionality.currentFolder = audioFileCard
-
-                            }) {
-                            Text(
-                                text = audioFileCard.name,
-                                fontSize = 18.sp,
-                                modifier = Modifier
-                                    .padding(16.dp)
-                                    .align(Alignment.CenterHorizontally)
-                            )
-                        }
-                    }
-                }
-                item {
-                    Spacer(modifier = Modifier.height(80.dp))
-                }
-
             }
+            item {
+                Spacer(modifier = Modifier.height(80.dp))
+            }
+
+        }
     }
     private fun getAudioFiles(folder: File): List<File> {
         val audioFiles = mutableListOf<File>()
@@ -154,13 +157,20 @@ class UI_views {
                                     )
 
                             }) {
-                            Text(
-                                text = audioFileCard.name,
-                                fontSize = 18.sp,
-                                modifier = Modifier
-                                    .padding(16.dp)
-                                    .align(Alignment.CenterHorizontally)
-                            )
+                            Row(modifier = Modifier
+                                .padding(16.dp)
+                                .align(Alignment.CenterHorizontally))
+                            {
+                                Text(
+                                    text = audioFileCard.name,
+                                    modifier = Modifier.widthIn(max = 320.dp),
+                                    fontSize = 18.sp,
+
+                                )
+                                Spacer(modifier = Modifier.weight(1f))
+                                TrackDropDownMenu()
+                            }
+
                         }
                     }
                 }
@@ -169,6 +179,26 @@ class UI_views {
                 }
             }
         )
-
     }
+    @Composable
+    fun TrackDropDownMenu()
+    {
+        var expanded by remember { mutableStateOf(false) }
+        Box {
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon(Icons.Default.MoreVert, contentDescription = "Open dropdown menu")
+            }
+
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.wrapContentSize()
+            ) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Add to next in queue")
+                }
+            }
+        }
+    }
+
 }
